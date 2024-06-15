a8e2316935b363e09b42143730c2e932efa14bed
DavidKeur <144221445+DavidKeur@users.noreply.github.com>
2024-06-15 20:57:15 +0200

    Update RF with correlation

--- ML/RandomForest_with_correlation.py
+++ ML/RandomForest_with_correlation.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-import pickle
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.utils import resample
 from sklearn.ensemble import RandomForestClassifier
@@ -23,14 +22,12 @@ from collections import Counter
 
 
 df = pd.read_csv("cleaned_df.csv")
-print(df["PKM2_inhibition"])
-print(df["ERK2_inhibition"])
 
 # 0) Correlation btw features
 df_corr = df.drop(["SMILES","PKM2_inhibition","ERK2_inhibition"], axis=1)
 correlation_matrix = df_corr.corr()
 
-# defined threshold for strong correlation
+# Defined threshold for strong correlation
 threshold = 0.8
 
 # Identify columns with a correlation higher than 0.8 and remove one of them
@@ -46,33 +43,18 @@ print(f"Colomns to remove: {to_remove}")
 df_reduced = df_corr.drop(columns=to_remove)
 df_reduced.to_csv("df_reduced.csv", index=False)
 
-
 ########################################################################
 # 1) Divide the dataset into X, Y for PKM2
-X_PKM2 = pd.read_csv("df_reduced.csv")
-X_ERK2 = pd.read_csv("df_reduced.csv")
 smiles = df['SMILES']
+X_PKM2 = pd.read_csv("df_reduced.csv")
 Y_PKM2 = df['PKM2_inhibition']
 
-# 2) Understand if the dataset is balanced  # ---> 0.03 => imbalanced
-class_distribution = Counter(Y_PKM2)
-print("Class distribution:", class_distribution)
-majority_class_count = class_distribution[0]
-minority_classes_count = sum(count for cls, count in class_distribution.items() if cls != 0)
-if minority_classes_count != 0:
-    imbalance_ratio = minority_classes_count / majority_class_count
-else:
-    imbalance_ratio = -999  # error, the denominator can't be 0
-print(f"Imbalance Ratio (majority:minority): {imbalance_ratio:.2f}")
-print("\n")
-
-# 3) Split the dataset into training and test sets for PKM2
+# 2) Split the dataset into training and test sets for PKM2
 X_train_PKM2, X_test_PKM2, Y_train_PKM2, Y_test_PKM2, smiles_train_PKM2, smiles_test_PKM2 = train_test_split(
     X_PKM2, Y_PKM2, smiles, test_size=0.30, stratify=Y_PKM2, random_state=190
 )
 
-
-# 4) Oversampling for PKM2
+# 3) Oversampling for PKM2
 majority_class_PKM2 = 0
 X_train_majority_PKM2 = X_train_PKM2[Y_train_PKM2 == majority_class_PKM2]
 Y_train_majority_PKM2 = Y_train_PKM2[Y_train_PKM2 == majority_class_PKM2]
@@ -86,7 +68,7 @@ Y_train_minority_resampled_PKM2 = resample(Y_train_minority_PKM2, replace=True, 
 X_train_resampled_PKM2 = pd.concat([X_train_majority_PKM2, X_train_minority_resampled_PKM2])
 Y_train_resampled_PKM2 = pd.concat([Y_train_majority_PKM2, Y_train_minority_resampled_PKM2])
 
-# 5) Random Forest for PKM2
+# 4) Random Forest for PKM2
 # Define the parameter grid
 parameters_PKM2 = {
     'n_estimators': [100, 110, 120, 130, 140],      # Number of trees in the forest
@@ -98,7 +80,7 @@ parameters_PKM2 = {
 }
 
 # Define the scoring matrix
-scoring_PKM2 = {'accuracy': 'accuracy', 'f1_micro': 'f1_micro', 'roc_auc': 'roc_auc','balanced_accuracy': 'balanced_accuracy', 'precision_micro': 'precision_micro'}
+scoring_PKM2 = {'accuracy': 'accuracy', 'f1_micro': 'f1_micro', 'roc_auc': 'roc_auc','balanced_accuracy': 'balanced_accuracy'}
 
 # Initialize the classifier
 classifier_PKM2 = RandomForestClassifier()
@@ -123,27 +105,15 @@ Y_pred_train_PKM2 = best_model_PKM2.predict(X_train_resampled_PKM2)
 ########################################################################
 # Repeat the process for ERK2
 # 1) Divide the dataset into X, Y for ERK2
+X_ERK2 = pd.read_csv("df_reduced.csv")
 Y_ERK2 = df['ERK2_inhibition']
 
-# 2) Understand if the dataset is balanced # ---> 0.05 => imbalanced
-class_distribution = Counter(Y_ERK2)
-print("\n")
-print("Class distribution:", class_distribution)
-majority_class_count = class_distribution[0]
-minority_classes_count = sum(count for cls, count in class_distribution.items() if cls != 0)
-if minority_classes_count != 0:
-    imbalance_ratio = minority_classes_count / majority_class_count
-else:
-    imbalance_ratio = -999  # error, the denominator can't be 0
-print(f"Imbalance Ratio (majority:minority): {imbalance_ratio:.2f}")
-print("\n")
-
-# 3) Split the dataset into training and test sets for ERK2
+# 2) Split the dataset into training and test sets for ERK2
 X_train_ERK2, X_test_ERK2, Y_train_ERK2, Y_test_ERK2, smiles_train_ERK2, smiles_test_ERK2 = train_test_split(
     X_ERK2, Y_ERK2, smiles, test_size=0.30, stratify=Y_ERK2, random_state=190
 )
 
-# 4) Oversampling for ERK2
+# 3) Oversampling for ERK2
 majority_class_ERK2 = 0
 X_train_majority_ERK2 = X_train_ERK2[Y_train_ERK2 == majority_class_ERK2]
 Y_train_majority_ERK2 = Y_train_ERK2[Y_train_ERK2 == majority_class_ERK2]
@@ -157,7 +127,7 @@ Y_train_minority_resampled_ERK2 = resample(Y_train_minority_ERK2, replace=True, 
 X_train_resampled_ERK2 = pd.concat([X_train_majority_ERK2, X_train_minority_resampled_ERK2])
 Y_train_resampled_ERK2 = pd.concat([Y_train_majority_ERK2, Y_train_minority_resampled_ERK2])
 
-# 5) Random Forest for ERK2
+# 4) Random Forest for ERK2
 # Define the parameter grid
 parameters_ERK2 = {
     'n_estimators': [100, 110, 120, 130, 140],      # Number of trees in the forest
@@ -169,7 +139,7 @@ parameters_ERK2 = {
 }
 
 # Define the scoring matrix
-scoring_ERK2 = {'accuracy': 'accuracy', 'f1_micro': 'f1_micro', 'roc_auc': 'roc_auc','balanced_accuracy': 'balanced_accuracy', 'precision_micro': 'precision_micro'}
+scoring_ERK2 = {'accuracy': 'accuracy', 'f1_micro': 'f1_micro', 'roc_auc': 'roc_auc','balanced_accuracy': 'balanced_accuracy'}
 
 # Initialize the classifier
 classifier_ERK2 = RandomForestClassifier()
@@ -190,35 +160,14 @@ best_model_ERK2 = gs_ERK2.best_estimator_
 Y_pred_test_ERK2 = best_model_ERK2.predict(X_test_ERK2)
 Y_pred_train_ERK2 = best_model_ERK2.predict(X_train_resampled_ERK2)
 
-# Save the model for ERK2
-with open('model_KNN_ERK2.pkl', 'wb') as file:
-    pickle.dump(best_model_ERK2, file)
-
-# Save the model for PKM2
-with open('model_KNN_PKM2.pkl', 'wb') as file:
-    pickle.dump(best_model_PKM2, file)
-
 
 ########################################################################
 # 6) Final results
+# Print classification report
 print("\nPKM2:")
 print(metrics.classification_report(Y_test_PKM2, Y_pred_test_PKM2))
 print("\nERK2:")
 print(metrics.classification_report(Y_test_ERK2, Y_pred_test_ERK2))
-
-# Quick validation of accuracy
-# Calculate the accuracy for PKM2 inhibition
-pkm2_correct_predictions = (Y_test_PKM2 == Y_pred_test_PKM2).sum()
-pkm2_total_predictions = len(Y_test_PKM2)
-pkm2_accuracy = pkm2_correct_predictions / pkm2_total_predictions
-
-# Calculate the accuracy for ERK2 inhibition
-erk2_correct_predictions = (Y_test_ERK2 == Y_pred_test_ERK2).sum()
-erk2_total_predictions = len(Y_test_ERK2)
-erk2_accuracy = erk2_correct_predictions / erk2_total_predictions
-
-print(f"\nPKM2 inhibition accuracy: {pkm2_accuracy:.2f}")
-print(f"ERK2 inhibition accuracy: {erk2_accuracy:.2f}")
 
 # Combine SMILES, actual and predicted values into a final dataframe for PKM2
 final_df_PKM2 = pd.DataFrame({
@@ -241,7 +190,6 @@ final_df = pd.merge(final_df_PKM2, final_df_ERK2, on='SMILES')
 print("\nFinal DataFrame with predictions and actual values:")
 print(final_df.head())
 
-
 # Additional metrics
 # Calculate F1-score, Precision, Recall, AUC-ROC, and (balanced) accuracy for PKM2 inhibition
 pkm2_accuracy = metrics.accuracy_score(Y_test_PKM2, Y_pred_test_PKM2)
@@ -259,7 +207,7 @@ print(f"PKM2 inhibition F1-score: {pkm2_f1:.2f}")
 print(f"PKM2 inhibition AUC-ROC: {pkm2_auc:.2f}")
 
 # Calculate F1-score, Precision, Recall, AUC-ROC, and (balanced) accuracy for PKM2 inhibition
-pkm2_accuracy = metrics.accuracy_score(Y_test_ERK2, Y_pred_test_ERK2)
+erk2_accuracy = metrics.accuracy_score(Y_test_ERK2, Y_pred_test_ERK2)
 erk2_bacc = metrics.balanced_accuracy_score(Y_test_ERK2, Y_pred_test_ERK2)
 erk2_precision = metrics.precision_score(Y_test_ERK2, Y_pred_test_ERK2)
 erk2_recall = metrics.recall_score(Y_test_ERK2, Y_pred_test_ERK2)
@@ -282,23 +230,8 @@ both_kinase_accuracy = both_kinase_correct / total_instances
 # Display Dual Kinase Accuracy
 print(f"\nBoth kinases correct (accuracy): {both_kinase_accuracy:.2f}")
 
-# Confusion matrix for PKM2
-cm_PKM2 = confusion_matrix(Y_test_PKM2, Y_pred_test_PKM2)
-sns.heatmap(cm_PKM2, annot=True, fmt='d', cmap="Blues")
-plt.title("PKM2 Confusion Matrix")
-plt.xlabel("Predicted Labels")
-plt.ylabel("True Labels")
-plt.show()
 
-# Confusion matrix for ERK2
-cm_ERK2 = confusion_matrix(Y_test_ERK2, Y_pred_test_ERK2)
-sns.heatmap(cm_ERK2, annot=True, fmt='d', cmap="Blues")
-plt.title("ERK2 Confusion Matrix")
-plt.xlabel("Predicted Labels")
-plt.ylabel("True Labels")
-plt.show()
-
-
+# Create AUC-ROC plots
 # Calculate ROC curve for PKM2
 y_probs_PKM2 = best_model_PKM2.predict_proba(X_test_PKM2)[:,1]  # Predict probabilities for the positive class
 fpr_PKM2, tpr_PKM2, thresholds_PKM2 = metrics.roc_curve(Y_test_PKM2, y_probs_PKM2)
